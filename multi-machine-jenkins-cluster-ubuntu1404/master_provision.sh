2fe8337e463a0d54a701fd9849d4f9be2a5ff100
Tian Xiao Ji <tianxj@cn.ibm.com>
2017-06-28 10:53:54 +0800

    Add docker support

--- multi-machine-jenkins-cluster-ubuntu1404/master_provision.sh
+++ multi-machine-jenkins-cluster-ubuntu1404/master_provision.sh
@@ -2,6 +2,8 @@
 #change apt source to be inside China to speed up apt
 #sudo mv /etc/apt/sources.list /etc/apt/sources.list.old
 #sudo cp /vagrant/sources.list /etc/apt/sources.list
+sudo cp /vagrant/ssh/id_rsa /home/ubuntu/.ssh/
+sudo cp /vagrant/ssh/id_rsa.pub /home/ubuntu/.ssh/
 sudo apt-get update
 
 #prepare for jenkins installation
@@ -20,4 +22,12 @@ sudo update-alternatives --config javac
 #install jenkins
 sudo apt-get -y install jenkins
 
+#install git
 sudo apt-get -y install git
+
+#install docker
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-key add /home/ubuntu/gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
--- multi-machine-jenkins-cluster-ubuntu1404/slave_provision.sh
+++ multi-machine-jenkins-cluster-ubuntu1404/slave_provision.sh
@@ -2,6 +2,10 @@
 #change apt source to be inside China to speed up apt
 #sudo mv /etc/apt/sources.list /etc/apt/sources.list.old
 #sudo cp /vagrant/sources.list /etc/apt/sources.list
+sudo cp /vagrant/ssh/id_rsa /home/ubuntu/.ssh/
+sudo cp /vagrant/ssh/id_rsa.pub /home/ubuntu/.ssh/
+sudo cat /vagrant/ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
+sudo cp /vagrant/docker/gpg /home/ubuntu/gpg
 sudo apt-get update
 #install openjdk
 #sudo apt-get -y install default-jdk
@@ -12,3 +16,11 @@ sudo update-alternatives --config java
 sudo update-alternatives --config javac
 
 sudo apt-get -y install git
+
+#install docker
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-key add /home/ubuntu/gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
